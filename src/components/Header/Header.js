8fb46a0cae79b88acf9539ed8b2c70082f24016e
asif2508 <47877578+asif2508@users.noreply.github.com>
2022-06-25 03:54:51 +0600

    header createed

---
+++ src/components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Container, Nav, Navbar} from 'react-bootstrap';
+const Header = () => {
+    return (
+        <div>
+            <Navbar bg="light" expand="lg">
+                <Container>
+                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ms-auto">
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#link">Link</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    );
+};
+
+export default Header;
